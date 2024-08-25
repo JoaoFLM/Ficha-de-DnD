d46ded0dfd18d162a84ea56b73541e0d71dd139f
Joao Felipe <lopesmonteirojoaofelipe@gmail.com>
2024-08-25 19:58:59 -0300

    Atributos feitos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,106 @@
 import Atributos.*
+import Raças.*
 import java.util.*
 import kotlin.system.exitProcess
 
 
 fun main() {
-    println("## Criação de personagem ##");
-    println("Digite o nome do personagem")
-    val EscolhaAUX  = Scanner(System.`in`)
-    val nome = EscolhaAUX.nextLine()
-    var PJ = Personagem(
-            nome,
-            Forca(),
-            Destreza(),
-            Constituicao(),
-            Inteligencia(),
-            Carisma(),
-            Sabedoria())
+    println("## Criação de personagem ##")
+    println("Digite o nome do personagem:")
+    val scanner = Scanner(System.`in`)
+    val nome = scanner.nextLine()
 
-    var pontos: Int = 0;
-    while(true){
-        PJ.MostrarAtributos()
+    println("Primeiro escolha a sua Raça:")
+    var raca: Raca? = null
+    var flag: Boolean
+    do {
+        println("Anao Da Colina [1]")
+        println("Anao Da Montanha [2]")
+        println("Alto Elfo [3]")
+        println("Elfo da Floresta [4]")
+        println("Drow [5]")
+        println("Hafling Pes Leves [6]")
+        println("Hafling Robusto [7]")
+        println("Humano [8]")
+        println("Gnomo da Floresta [9]")
+        println("Gnomo das Rochas [10]")
+        println("Draconato [11]")
+        println("Meio-Elfo [12]")
+        println("Meio-Orc [13]")
+        println("Tiefling [14]")
+
+        val escolhaRaca = scanner.nextInt()
+        raca = when (escolhaRaca) {
+            1 -> AnaoDaColina()
+            2 -> AnaoDaMontanha()
+            3 -> AltoElfo()
+            4 -> ElfoDaFloresta()
+            5 -> Drow()
+            6 -> HaflingPesLeves()
+            7 -> HaflingRobusto()
+            8 -> Humano()
+            9 -> GnomoDaFloresta()
+            10 -> GnomoDasRochas()
+            11 -> Draconato()
+            12 -> MeioElfo()
+            13 -> MeioOrc()
+            14 -> Tiefling()
+            else -> {
+                println("Opção inválida. Tente novamente.")
+                null
+            }
+        }
+        flag = raca == null
+    } while (flag)
+
+    val personagem = Personagem(
+        nome,
+        Forca(),
+        Destreza(),
+        Constituicao(),
+        Inteligencia(),
+        Carisma(),
+        Sabedoria(),
+        raca!!
+    )
+
+    var pontos = 0
+    val maxPontos = 27
+
+    while (true) {
+        personagem.MostrarAtributos()
         println("Escolha qual atributo deseja gastar pontos:")
-        println("Forca: [1]")
+        println("Força: [1]")
         println("Destreza: [2]")
         println("Constituição: [3]")
         println("Inteligência: [4]")
         println("Sabedoria: [5]")
         println("Carisma: [6]")
-        println("Sair [0]")
-        println("## Pontos restantes: $pontos/27 ##")
-        val attAUX: Int = EscolhaAUX.nextInt()
+        println("## Pontos restantes: ${maxPontos - pontos}/$maxPontos ##")
 
-        println("Digite o valor do atributo")
-        val pontoAUX: Int = EscolhaAUX.nextInt()
+        val escolhaAtributo = scanner.nextInt()
 
-        when(attAUX){
-            1 -> pontos += PJ.ComprarAtributos(PJ.ForcaPJ, pontoAUX)
-            2 -> pontos += PJ.ComprarAtributos(PJ.DestrezaPJ, pontoAUX)
-            3 -> pontos += PJ.ComprarAtributos(PJ.ConstituicaoPJ, pontoAUX)
-            4 -> pontos += PJ.ComprarAtributos(PJ.InteligenciaPJ, pontoAUX)
-            5 -> pontos += PJ.ComprarAtributos(PJ.SabedoriaPJ, pontoAUX)
-            6 -> pontos += PJ.ComprarAtributos(PJ.CarismaPJ, pontoAUX)
-            0 -> exitProcess(0)
-            else -> println("Atributo não encontrado");
+        println("Digite o valor do atributo:")
+        val valorAtributo = scanner.nextInt()
+
+        when (escolhaAtributo) {
+            1 -> pontos += personagem.ComprarAtributos(personagem.ForcaPJ, valorAtributo)
+            2 -> pontos += personagem.ComprarAtributos(personagem.DestrezaPJ, valorAtributo)
+            3 -> pontos += personagem.ComprarAtributos(personagem.ConstituicaoPJ, valorAtributo)
+            4 -> pontos += personagem.ComprarAtributos(personagem.InteligenciaPJ, valorAtributo)
+            5 -> pontos += personagem.ComprarAtributos(personagem.SabedoriaPJ, valorAtributo)
+            6 -> pontos += personagem.ComprarAtributos(personagem.CarismaPJ, valorAtributo)
+            else -> println("Atributo não encontrado")
+        }
+
+        if (pontos > maxPontos) {
+            println("Limite de pontos ultrapassado")
+            pontos -= valorAtributo
+        } else if (pontos == maxPontos) {
+            personagem.AdicionaRaca()
+            break
         }
     }
+    personagem.MostarPersonagem()
+    scanner.close()
 }
--- src/main/kotlin/Personagem.kt
+++ src/main/kotlin/Personagem.kt
@@ -1,4 +1,5 @@
 import Atributos.*
+import Raças.Raca
 import java.util.Scanner
 
 
@@ -11,6 +12,7 @@ class Personagem (
     var InteligenciaPJ: Inteligencia,
     var CarismaPJ: Carisma,
     var SabedoriaPJ: Sabedoria,
+    var Raca: Raca
     )
     {
         fun ComprarAtributos(atributo: Atributo, pontosGastos: Int) : Int {
@@ -19,12 +21,50 @@ class Personagem (
 
         fun MostrarAtributos()
         {
-            println("Atributos atuais:")
+            println("## Atributos ##")
             println("Forca: ${ForcaPJ.att}")
             println("Destreza: ${DestrezaPJ.att}")
             println("Constituição: ${ConstituicaoPJ.att}")
             println("Intelingência: ${InteligenciaPJ.att}")
             println("Carisma: ${CarismaPJ.att}")
             println("Sabedoria: ${SabedoriaPJ.att}")
+        }
+
+        private fun CalculaPV(Constituicao: Int) : Int? {
+            val PVbase = 10
+            when (Constituicao){
+                8 -> return PVbase-1
+                9 -> return PVbase-1
+                10 -> return PVbase+0
+                11 -> return PVbase+0
+                12 -> return PVbase+1
+                13 -> return PVbase+1
+                14 -> return PVbase+2
+                15 -> return PVbase+2
+                16 -> return PVbase+3
+                17 -> return PVbase+3
+                18 -> return PVbase+4
+                19 -> return PVbase+4
+                20 -> return PVbase+5
+            }
+            return null
+        }
+
+        fun AdicionaRaca(){
+            this.ForcaPJ.att+=this.Raca.IncrementoForca
+            this.DestrezaPJ.att+=this.Raca.IncrementoDestreza
+            this.ConstituicaoPJ.att+=this.Raca.IncrementoConstituicao
+            this.InteligenciaPJ.att+=this.Raca.IncrementoInteligencia
+            this.SabedoriaPJ.att+=this.Raca.IncrementoSabedoria
+            this.CarismaPJ.att+=this.Raca.IncrementoCarisma
+        }
+
+        fun MostarPersonagem()
+        {
+            println("## $Nome ##")
+            println("Pontos de Vida: ${CalculaPV(this.ConstituicaoPJ.att)}")
+            println("Raca ${Raca.RacaNome}")
+            this.MostrarAtributos()
+
         }
     }
